510589aa4744d4b9a046ba65fd33adc23f30e56f
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 14:09:11 +0200

    shaman + warrior
    german description of shaman and warrior

--- loc/classes/shaman.js
+++ loc/classes/shaman.js
@@ -8,6 +8,7 @@ export default {
   // Class description, visible during character creation
   description: {
     en: 'The Shamans role is to support the group by healing the Tank and offering offensive Buffs or crippling Debuffs.',
+    de: 'Die Rolle des Schamanen in der Gruppe ist es, den Tank zu heilen und offensive Buffs oder lähmende Debuffs zu verteilen.',
     se: 'Schamanens roll är att hjälpa sin grupp med att hela upp Tanken och använda sig utav sina offensiva Buffs eller förlamande Debuffs.'
   }
 }
--- loc/classes/warrior.js
+++ loc/classes/warrior.js
@@ -8,6 +8,7 @@ export default {
   // Class description, visible during character creation
   description: {
     en: 'Warriors are built to tank more monsters than any other class by having high defense through equipment (Shields) and strong defensive buffs.',
+    de: 'Krieger sind gemacht dafür mehr Monster abzuhalten als alle anderen Klassen, da sie hohe Verteidigung durch ihre Ausrüstung (Schilde) und starke, defensive Buffs haben.',
     se: 'Krigare är byggda för att tåla mer skada än alla andra klasser genom högt försvar från sin utrustning (Sköld) och starka försvars Buffar.'
   }
 }

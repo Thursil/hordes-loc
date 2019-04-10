01d18d1e1f3b9aa346d91df0a92f9ffa1e335148
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 07:25:20 +0200

    mage, german
    mage, german desc

---
+++ mage.js
@@ -0,0 +1,14 @@
+export default {
+  // The class name
+  name: {
+    en: 'Mage',
+    de: 'Magier',
+    se: 'Magiker'
+  },
+  // Class description, visible during character creation
+  description: {
+    en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
+    de: 'Magier richten sehr starken und großflächigen Schaden an, während Sie ihre Gegner mithilfe von Eismagie verlangsamen.',
+    se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.'
+  }
+}

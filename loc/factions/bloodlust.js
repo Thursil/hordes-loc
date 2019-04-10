27a6584084b411f6f2c0f363e04d92e3cf949d19
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 14:55:49 +0200

    bloodlust german translation
    added german translation for bloodlust

--- loc/factions/bloodlust.js
+++ loc/factions/bloodlust.js
@@ -1,10 +1,12 @@
 export default {
   name: {
     en: 'Bloodlust',
+    de: 'Blutlust',
     se: 'Bloodlust'
   },
   description: {
     en: 'The Bloodlust horde values freedom and loyalty, offers a place for individualism or even chaos. Their Defenses are located in a desert realm called Headless Landing.',
+    de: 'Die Blutlust Horde legt Wert auf Freiheit und Loyalität, außerdem bietet sie einen Platz für Individualismus oder sogar Chaos. Ihre Verteidigung ist in einer Wüste namens Kopflose Landung.',
     se: 'Bloodlust värderar frihet och lojalitet, erbjunder en plats för individualism men också kaos. Deras försvar hittas i ett öken kungarike vid namn Headless Landing.'
     
   }

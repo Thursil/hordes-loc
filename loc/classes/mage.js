41ee13e98d1d6c538cd752839a62f1d95971dbff
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 14:11:09 +0200

    Update mage.js
    little spelling error fixed

--- loc/classes/mage.js
+++ loc/classes/mage.js
@@ -8,7 +8,7 @@ export default {
   // Class description, visible during character creation
   description: {
     en: 'Mages deal very heavy large scale Area of Effect damage while also providing utility through slowing targets with their ice based magic.',
-    de: 'Magier richten sehr starken und großflächigen Schaden an, während Sie ihre Gegner mithilfe von Eismagie verlangsamen.',
+    de: 'Magier richten sehr starken und großflächigen Schaden an, während sie ihre Gegner mithilfe von Eismagie verlangsamen.',
     se: 'Magiker gör mycket hög areal skada och har även hjälp av sin is baserade magi för att kunna göra sina motståndare långsamma.'
   }
 }

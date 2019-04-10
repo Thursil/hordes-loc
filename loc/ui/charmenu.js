1e24888f0ddaab8cd4929cc8c8d5c55c970686a2
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 14:34:43 +0200

    charmenu german
    german description for charmenu

--- loc/ui/charmenu.js
+++ loc/ui/charmenu.js
@@ -5,22 +5,27 @@ export default {
   select: {
     title: {
       en: 'Select Character',
+      de: 'Wähle Charakter',
       se: 'Välj Karaktär'
     },
     create: {
       en: 'Click to create character',
+      de: 'Klicke um einen Charakter zu erschaffen',
       se: 'Klicka för att skapa karaktär'
     },
     enterWorld: {
       en: 'Enter World',
+      de: 'Betritt Welt',
       se: 'Starta Värld'
     },
     emptySlot: {
       en: 'Empty',
+      de: 'Leer',
       se: 'Ingen'
     },
     error: {
       en: 'Network error',
+      de: 'Netzwerkproblem',
       se: 'Nätverks problem'
     }
   },
@@ -29,26 +34,32 @@ export default {
   create: {
     title: {
       en: 'Character creation',
+      de: 'Charaktererstellung',
       se: 'Skapa karaktär'
     },
     pressIcon: {
       en: 'Press an icon for a brief description.',
+      de: 'Klicke auf ein Symbol für eine kurze Beschreibung',
       se: 'Klicka en ikon för beskrivning.'
     },
     selectClass: {
       en: 'Select a class',
+      de: 'Wähle eine Klasse',
       se: 'Välj en klass'
     },
     selectFaction: {
       en: 'Select a faction',
+      de: 'Wähle eine Fraktion',
       se: 'Välj en sida'
     },
     enterName: {
       en: 'Enter a name',
+      de: 'Gib einen Namen ein',
       se: 'Välj ett namn'
     },
     nameReq: {
       en: 'Your name can only contain characters a-Z, and numbers 0-9, without any spaces. Up to 16 characters.',
+      de: 'Dein Name kann nur Buchstaben a-Z und Zahlen 0-9 ohne Leerzeichen beinhalten. Maximal 16 Zeichen.',
       se: 'Ditt namn kan bara använda sig karaktärerna a-Z, och nummer 0-9, utan mellanslag. Max 16 karaktärer.'
     }
   },
@@ -58,14 +69,17 @@ export default {
     // It places the characters name after the string, e.g. 'Delete Battlebubbles'
     title: {
       en: 'Delete',
+      de: 'Lösche',
       se: 'Ta bort'
     },
     info: {
       en: 'To delete, type the characters name. Your character can not be restored.',
+      de: 'Schreibe den Namen des Charakters, den du löschen willst. Er kann nicht wiederhergestellt werden.',
       se: 'För att ta bort, skriv in karaktärens namn. Din karaktär tas bort för alltid.'
     },
     placeholder: {
       en: 'Are you sure?',
+      de: 'Bist du sicher?',
       se: 'Är du säker?'
     }
   }

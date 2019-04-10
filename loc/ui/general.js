712e9b5602e959f4684cfeefb8ffeecf5ddd34ee
Thursil <Thursil.Overlord@gmail.com>
2019-04-10 14:37:16 +0200

    german translation general
    german translation for general

--- loc/ui/general.js
+++ loc/ui/general.js
@@ -2,10 +2,12 @@ export default {
   // Simple basic UI elements that pop up everywhere
   ok: {
     en: 'Okay',
+    de: 'Okay',
     se: 'Okej'
   },
   back: {
     en: 'Back',
+    de: 'Zurück',
     se: 'Bakåt'
   }
 }
